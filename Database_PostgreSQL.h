b8a79044b782bd16918d47e4f0014f949f906220
Milan Laslop <milan33@gmail.com>
2014-05-19 21:51:08 +0200

    override keyword for virtual methods

--- Database_PostgreSQL.h
+++ Database_PostgreSQL.h
@@ -9,18 +9,18 @@ class Database_PostgreSQL : public Database {
 public:
 	Database_PostgreSQL(const Config_Db &config, Temp &temp);
 
-	virtual std::set<std::string> getTables();
-	virtual void exportTable(const std::string &tableName, const boost::filesystem::path &file);
-	virtual void exportData(const std::string &tableName, const std::string &ignoreWhere, const boost::filesystem::path &file);
-	virtual void printDeleteTable(const std::string &tableName, const boost::filesystem::path &file);
-	virtual void import(const boost::filesystem::path &file);
-	virtual void deleteTable(const std::string &tableName);
-	virtual std::set<std::string> getTableDependencies(const std::string &tableName);
-	virtual bool isVersioned();
-	virtual void makeVersioned();
-	virtual void makeNotVersioned();
-	virtual uint32_t getVersion();
-	virtual void setVersion(const uint32_t version);
+	virtual std::set<std::string> getTables() override;
+	virtual void exportTable(const std::string &tableName, const boost::filesystem::path &file) override;
+	virtual void exportData(const std::string &tableName, const std::string &ignoreWhere, const boost::filesystem::path &file) override;
+	virtual void printDeleteTable(const std::string &tableName, const boost::filesystem::path &file) override;
+	virtual void import(const boost::filesystem::path &file) override;
+	virtual void deleteTable(const std::string &tableName) override;
+	virtual std::set<std::string> getTableDependencies(const std::string &tableName) override;
+	virtual bool isVersioned() override;
+	virtual void makeVersioned() override;
+	virtual void makeNotVersioned() override;
+	virtual uint32_t getVersion() override;
+	virtual void setVersion(const uint32_t version) override;
 
 private:
 	std::set<std::string> getTables_internal();
